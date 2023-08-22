aaa7ad4aeee45a8c4538f64cf654429edbe93948
IoIxD <alphaproject217@gmail.com>
2023-08-22 12:57:08 -0700

    updated examples a bit. updated readmE

--- examples/block-break-event/src/lib.rs
+++ examples/block-break-event/src/lib.rs
@@ -1,26 +1,25 @@
+use std::error::Error;
+
 use blackboxmc_general::JNIInstantiatable;
 use blackboxmc_general::SharedJNIEnv;
 use jni::{objects::JObject, JNIEnv};
 
 #[no_mangle]
 pub extern "system" fn __on__BlockBreakEvent(env: JNIEnv<'_>, obj: JObject<'_>) {
-    let e = SharedJNIEnv::new(env);
-    let event = blackboxmc_bukkit::event::block::BlockBreakEvent::from_raw(&e, obj).unwrap();
+    let res = || -> Result<(), Box<dyn Error>> {
+        let e = SharedJNIEnv::new(env);
+        let event = blackboxmc_bukkit::event::block::BlockBreakEvent::from_raw(&e, obj)?;
 
-    // Cancel the event.
-    event.set_cancelled(true).expect("Couldn't cancel event");
+        // Cancel the event.
+        event.set_cancelled(true)?;
 
-    let player = event.player().expect("Couldn't get player");
-    if let Some(player) = player {
-        println!(
-            "{}",
-            player
-                .inventory()
-                .expect("Couldn't get inventory")
-                .item_in_hand()
-                .expect("Couldn't get item in hand")
-                .get_type()
-                .expect("Couldn't get type")
-        );
+        let player = event.player()?;
+        if let Some(player) = player {
+            println!("{}", player.inventory()?.item_in_main_hand()?.get_type()?);
+        };
+        Ok(())
+    }();
+    if let Err(err) = res {
+        println!("BlockBreakEvent error: {}", err);
     }
 }
--- examples/mc-classic-mode/src/lib.rs
+++ examples/mc-classic-mode/src/lib.rs
@@ -1,12 +1,8 @@
 use blackboxmc_general::JNIInstantiatable;
-use std::{cell::OnceCell, error::Error, fmt::Display, ops::Deref, sync::Mutex, time::SystemTime};
+use std::error::Error;
 
 use blackboxmc_bukkit::{
-    command::{Command, CommandSender},
-    event::server::PluginEnableEvent,
-    generator::{ChunkGenerator, ChunkGeneratorChunkData, WorldInfo},
-    plugin::Plugin,
-    scheduler::BukkitRunnable,
+    event::server::PluginEnableEvent, plugin::Plugin, scheduler::BukkitRunnable,
 };
 use blackboxmc_general::SharedJNIEnv;
 use jni::{objects::JObject, sys::jint, JNIEnv};
@@ -27,9 +23,9 @@ impl HungerThread {
         )
     }
     pub fn run(plug: &mut Plugin) -> Result<(), Box<dyn Error>> {
-        let mut server = plug.server()?;
+        let server = plug.server()?;
         let players = server.online_players()?;
-        for mut player in players {
+        for player in players {
             println!("{}", player.name()?);
         }
         Ok(())
@@ -38,10 +34,10 @@ impl HungerThread {
 
 #[no_mangle]
 pub extern "system" fn __extends__BukkitRunnable__HungerThread__run<'mc>(
-    mut env: JNIEnv<'mc>,
-    address: jint,
+    env: JNIEnv<'mc>,
+    _address: jint,
     plugin: JObject<'mc>,
-    objs: JObject<'mc>,
+    _objs: JObject<'mc>,
 ) {
     let e = SharedJNIEnv::new(env);
     let mut plug = Plugin::from_raw(&e, plugin).unwrap();
@@ -52,15 +48,15 @@ pub extern "system" fn __extends__BukkitRunnable__HungerThread__run<'mc>(
 pub extern "system" fn __on__PluginEnableEvent(env: JNIEnv<'_>, obj: JObject<'_>) {
     color_eyre::install().expect("Java exception");
     let e = SharedJNIEnv::new(env);
-    let mut ev1 = PluginEnableEvent::from_raw(&e, unsafe { JObject::from_raw(obj.clone()) })
+    let ev1 = PluginEnableEvent::from_raw(&e, unsafe { JObject::from_raw(obj.clone()) })
         .expect("Java exception");
-    let mut ev2 = PluginEnableEvent::from_raw(&e, obj).expect("Java exception");
+    let ev2 = PluginEnableEvent::from_raw(&e, obj).expect("Java exception");
 
     let plug1 = ev1.plugin().expect("Java exception");
     let plug2 = ev2.plugin().expect("Java exception");
     // New runnable
     println!("run task timer");
-    let mut runnable = HungerThread::new(&e, &plug1).expect("Java exception");
+    let runnable = HungerThread::new(&e, &plug1).expect("Java exception");
     runnable
         .run_task_timer(plug2, 0, 20)
         .expect("Java exception");
